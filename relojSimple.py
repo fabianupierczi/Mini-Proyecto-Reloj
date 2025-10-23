e8e5f7b635bcf9fcf60ecac8980986a34a7326e6
fabianupierczi <fabianupierczi@gmail.com>
2025-10-22 21:58:43 -0300

    Agregado Boton de Salida y DNI

--- relojSimple.py
+++ relojSimple.py
@@ -11,8 +11,17 @@ def hora():
     reloj.config(text = tiempo_actual)
     ventana.after(1000, hora)
 
+labelDNI = tk.Label(ventana, text="Ingrese su DNI")
+labelDNI.place(x=50, y=100)
+
+entryDNI = tk.Entry()
+entryDNI.place(x=50, y=120)
+
 botonEntrada = tk.Button(text="ENTRADA")
 botonEntrada.place(x=50, y=150)
+
+botonSalida = tk.Button(text="SALIDA")
+botonSalida.place(x=210, y=150)
 
 reloj.pack(anchor = 'center')
 hora()
